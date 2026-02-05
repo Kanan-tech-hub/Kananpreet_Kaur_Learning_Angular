3756e6f594b5c9874554dcabe85325341326104f
KANAN <W0878434@MYSCC.CA>
2026-02-05 16:48:32 -0500

    Create food-list and food-list-item components

---
+++ src/app/food-list-item/food-list-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-list-item',
+  imports: [],
+  templateUrl: './food-list-item.html',
+  styleUrl: './food-list-item.css',
+})
+export class FoodListItem {
+
+}
