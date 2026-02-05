40067fc7d09808357073561087c76f1ae638ce1b
KANAN <W0878434@MYSCC.CA>
2026-02-05 16:40:55 -0500

    Add movie-list and movie-list-item components

---
+++ src/app/movie-list-item/movie-list-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list-item',
+  imports: [],
+  templateUrl: './movie-list-item.html',
+  styleUrl: './movie-list-item.css',
+})
+export class MovieListItem {
+
+}
