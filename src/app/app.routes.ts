259be7da1c91b8275f16c59fc2766d2145e08d4d
kanantechhub <w0878434@myscc.ca>
2026-02-10 13:48:59 -0500

    added application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./food-list/food-list.component')
+        .then(m => m.FoodListComponent)
+  },
+  {
+    path: 'modify',
+    loadComponent: () =>
+      import('./modify-list-item/modify-list-item.component')
+        .then(m => m.ModifyListItemComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./page-not-found/page-not-found.component')
+        .then(m => m.PageNotFoundComponent)
+  }
+];
